0b5f1a418e7c8fe79cce4a4f6b5ff61f92a6895e
Kakesn <mnelson55@mail.csuchico.edu>
2017-10-18 17:02:14 -0700

    StarMatrix

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{ 
+
+    vector < vector < char > > Matrix;
+    int row;
+    int col;
+    char star;
+
+    cout<<" Enter rows and columns of desired stars" <<endl;
+    cin>> row;
+    cin>> col;
+
+    if(row > 0 && col > 0)
+    {
+        Matrix.resize(row);
+        for(int i = 0; i < row; i++)
+        {
+            Matrix.resize(col);
+            for(int j = 0; j < col; j++)
+            {
+                cout<< '*';
+            }   cout<< endl;
+        }
+    }
+
+
+return 0;
+}
